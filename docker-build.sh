953cb6b37de46a3eec9c5aeacb8c1182677a862e
gityh2021 <85598202+gityh2021@users.noreply.github.com>
2022-06-06 15:44:44 +0800

    修复Go download下载慢的问题

--- docker-build.sh
+++ docker-build.sh
@@ -1,6 +1,6 @@
 docker build -t fduyh2021/video:v1 -f VideoDockerfile .
 docker build -t fduyh2021/user:v1 -f UserDockerfile .
 docker build -t fduyh2021/api:v1 -f ApiDockerfile .
-docker run -p 8080:8080 --network my_net fduyh2021/api:v1
-docker run -p 8888:8081 --network my_net fduyh2021/video:v1
-docker run -p 8888:8888 --network my_net fduyh2021/user:v1
+docker run -p 8080:8082 --network douyin fduyh2021/api:v1
+docker run -p 8081:8081 --network douyin fduyh2021/video:v1
+docker run -p 8889:8889 --network douyin fduyh2021/user:v1
