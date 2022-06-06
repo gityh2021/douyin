1feb795f114608e77ced35853b361f80211291bc
gityh2021 <85598202+gityh2021@users.noreply.github.com>
2022-06-07 01:25:06 +0800

    修改数据库中文乱码

--- docker-build.sh
+++ docker-build.sh
@@ -1,6 +1,6 @@
 docker build -t fduyh2021/video:v1 -f VideoDockerfile .
 docker build -t fduyh2021/user:v1 -f UserDockerfile .
 docker build -t fduyh2021/api:v1 -f ApiDockerfile .
-docker run --name video -d -p 8088:8088 --network douyin fduyh2021/video:v1
-docker run --name user -d -p 8087:8087 --network douyin fduyh2021/user:v1
+docker run --name video -p 8088:8088 --network douyin fduyh2021/video:v1
+docker run --name user -p 8087:8087 --network douyin fduyh2021/user:v1
 docker run --name api -p 8082:8082 --network douyin fduyh2021/api:v1
