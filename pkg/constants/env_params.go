8b7aab3ac80218846322117bde75a0d371d2d30d
gityh2021 <85598202+gityh2021@users.noreply.github.com>
2022-06-07 21:04:36 +0800

    优化创建视频

--- pkg/constants/env_params.go
+++ pkg/constants/env_params.go
@@ -1,13 +1,15 @@
 package constants
 
-//// 部署环境
-//var NetworkAddress = os.Getenv("NETWORK_IP")
-//var USER_PORT = os.Getenv("USER_PORT")
-//var VIDEO_PORT = os.Getenv("VIDEO_PORT")
-//var API_PORT = os.Getenv("API_PORT")
+import "os"
 
-// Debug环境
-var NetworkAddress = "127.0.0.1"
-var USER_PORT = "8087"
-var VIDEO_PORT = "8088"
-var API_PORT = "8082"
+// 部署环境
+var NetworkAddress = os.Getenv("NETWORK_IP")
+var USER_PORT = os.Getenv("USER_PORT")
+var VIDEO_PORT = os.Getenv("VIDEO_PORT")
+var API_PORT = os.Getenv("API_PORT")
+
+//// Debug环境
+//var NetworkAddress = "127.0.0.1"
+//var USER_PORT = "8087"
+//var VIDEO_PORT = "8088"
+//var API_PORT = "8082"
